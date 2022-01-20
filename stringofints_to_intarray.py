431a29a06f1769724ef582c9beb0c4297e5ec297
ganeshdevisettin <33672898+ganeshdevisettin@users.noreply.github.com>
2022-01-20 09:56:54 -0500

    Update stringofints_to_intarray.py

--- stringofints_to_intarray.py
+++ stringofints_to_intarray.py
@@ -1,4 +1,5 @@
 def make_arr(message):
+    # Enter string of ints, with a space between each integer
     new_arr = input(f"Enter values for {message}: ").split()
     for i in range(len(new_arr)):
         new_arr[i] = int(new_arr[i])
